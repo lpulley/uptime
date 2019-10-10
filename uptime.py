d511f7e30250c06107c91de336710c0b0682936c
Logan Pulley <logan@pulley.host>
2019-10-10 09:53:41 -0500

    Initial commit

---
+++ uptime.py
@@ -0,0 +1,21 @@
+import re
+import numpy as np
+from matplotlib import pyplot as plt
+from datetime import datetime
+import subprocess
+
+subprocess.run(['scp', 'raven:uptime.log', '.'])
+
+load = {}
+for i in range(24):
+    load[i] = np.array([])
+
+with open('uptime.log') as file:
+    for line in file:
+        timestamp = ':'.join([l for l in re.split(':|,| |\n', line)[:4] if l])
+        dt = datetime.strptime(timestamp, '%H:%M:%S')
+        fl = float([l for l in re.split(':|,| |\n', line)[-6:] if l][2])
+        load[dt.hour] = np.append(load[dt.hour], fl)
+
+plt.boxplot([load[i] for i in range(24)], showfliers=False)
+plt.savefig('load.png')
