7ddfc9c47dc8a02b7cfe9bfee0d9b76edda26133
Logan Pulley <logan@pulley.host>
2019-10-10 10:29:08 -0500

    Configured uptime.py to be a better package

--- uptime.py
+++ uptime.py
@@ -2,20 +2,38 @@ import re
 import numpy as np
 from matplotlib import pyplot as plt
 from datetime import datetime
-from subprocess import run
+import subprocess
+import sys
 
-run(['scp', 'raven:uptime.log', '.'])
+SSH_HOST = 'raven'
+LOG_NAME = 'uptime.log'
+PLOT_FORMAT = 'png'
 
-load = {}
-for i in range(24):
-    load[i] = np.array([])
+def getLog():
+    try:
+        subprocess.run(['scp', '{}:{}'.format(SSH_HOST, LOG_NAME), '.'], check=True)
+        return 0
+    except subprocess.CalledProcessError as e:
+        return e.returncode
 
-with open('uptime.log') as file:
-    for line in file:
-        timestamp = ':'.join([l for l in re.split(':|,| |\n', line)[:4] if l])
-        dt = datetime.strptime(timestamp, '%H:%M:%S')
-        fl = float([l for l in re.split(':|,| |\n', line)[-6:] if l][2])
-        load[dt.hour] = np.append(load[dt.hour], fl)
+def genHourlyLoad():
+    load = {}
+    for i in range(24):
+        load[i] = np.array([])
 
-plt.boxplot([load[i] for i in range(24)], showfliers=False)
-plt.savefig('load.png')
+    with open(LOG_NAME) as file:
+        for line in file:
+            timestamp = ':'.join([l for l in re.split(':|,| |\n', line)[:4] if l])
+            dt = datetime.strptime(timestamp, '%H:%M:%S')
+            fl = float([l for l in re.split(':|,| |\n', line)[-6:] if l][2])
+            load[dt.hour] = np.append(load[dt.hour], fl)
+
+    plt.boxplot([load[i] for i in range(24)], showfliers=False, labels=range(24))
+    plt.savefig('load.{}'.format(PLOT_FORMAT))
+
+if __name__ == '__main__':
+    scp_code = getLog()
+    if scp_code == 0:
+        genHourlyLoad()
+    else:
+        sys.exit('Couldn\'t access uptime.log over SCP: scp exited with {}'.format(scp_code))
